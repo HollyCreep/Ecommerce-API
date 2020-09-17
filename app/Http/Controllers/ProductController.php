a2a18b2598745b9a7b07f3a4ab7a4903c4354bc8
HollyCreep <lucasferreirarm@gmail.com>
2020-09-17 19:36:42 -0300

    Salvando progreso home

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // return Product::getAll();
+
+        return [
+            [
+                "referencia" => "123",
+                "descricao" => "Teste",
+                "categoria" => "Categoria",
+                "genero" => "Masculino",
+                "tam_min" => "32",
+                "tam_max" => "36",
+                "preco" => "12.50",
+                "tabelaPreco" => "12.50",
+                "materialCabedal" => "couro",
+                "materialSolado" => "borracha",
+                "alturaSolado" => "12",
+                "switchPromocao" => false,
+                "switchPremio" => false,
+                "fotosProduto" => []
+            ],
+            [
+                "referencia" => "1234",
+                "descricao" => "Teste1",
+                "categoria" => "Categoria",
+                "genero" => "Masculino",
+                "tam_min" => "32",
+                "tam_max" => "36",
+                "preco" => "12.50",
+                "tabelaPreco" => "12.50",
+                "materialCabedal" => "couro",
+                "materialSolado" => "borracha",
+                "alturaSolado" => "12",
+                "switchPromocao" => false,
+                "switchPremio" => false,
+                "fotosProduto" => []
+            ],
+            [
+                "referencia" => "1235",
+                "descricao" => "Teste1",
+                "categoria" => "Categoria",
+                "genero" => "Masculino",
+                "tam_min" => "32",
+                "tam_max" => "36",
+                "preco" => "12.50",
+                "tabelaPreco" => "12.50",
+                "materialCabedal" => "couro",
+                "materialSolado" => "borracha",
+                "alturaSolado" => "12",
+                "switchPromocao" => false,
+                "switchPremio" => false,
+                "fotosProduto" => []
+            ]
+        ];
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        //
+    }
+}
